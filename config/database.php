01ce4973dbe949f27ff23dd0e9c62422909abccb
Bernardo <bmgg358@gmail.com>
2016-10-11 00:14:23 -0400

    env

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('P_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('P_HOST', 'localhost'),
+            'port' => env('P_PORT', '5432'),
+            'database' => env('P_DATABASE', 'forge'),
+            'username' => env('P_USERNAME', 'forge'),
+            'password' => env('P_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',

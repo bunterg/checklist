884c212dc57d8fc10f60335690c7ac7fa3044575
Bernardo <bmgg358@gmail.com>
2016-10-14 19:19:05 -0400

    delete action

--- app/Cadena.php
+++ app/Cadena.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cadena extends Model
 {
+    use SoftDeletes;
     //
     protected $description = ['description'];
     protected $version = ['version'];
     protected $name = ['name'];
     protected $id = ['id'];
+    protected $dates = ['deleted_at'];
 }

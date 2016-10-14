5ecd3c086eaa0e92cd5115e0b255e831126f6900
Bernardo <bmgg358@gmail.com>
2016-10-14 19:16:32 -0400

    probando boton eliminar

--- resources/views/administ/editor.blade.php
+++ resources/views/administ/editor.blade.php
@@ -14,9 +14,13 @@
             </textarea>
           </div>
           <div class="panel-footer">
-            <button type="submit" class="btn btn-danger">
-              <span class="glyphicon glyphicon-trash"></span>
-            </button>
+            <form method="POST">
+              {{ csrf_token() }}
+              <input name="_method" type="hidden" value="DELETE">
+              <button type="submit" class="btn btn-danger">
+                <span class="glyphicon glyphicon-trash"></span>
+              </button>
+            </form>
           </div>
         </div>
 	  </div>    
